e6a50e3d930a3c9ef0de941b268e758bfb9774c8
vladstamatin <vladstmatin@yahoo.com>
2020-02-04 10:33:39 +0200

    fixed issue when iterate transactions

--- Connect.rb
+++ Connect.rb
@@ -36,43 +36,31 @@ class GetAccountsData
     browser = Watir::Browser.new :firefox
     browser.goto 'http://demo.bendigobank.com.au'
     browser.button(name: 'customer_type').click
-
     #parse accounts related data and iterate over it
     accounts_list = browser.ol(class: 'grouped-list__group__items')
     accounts_list.lis.each do |li|
-      #navigate to select last month for transactions
+      #navigate to our account and select last month for transactions
       li.a(class: 'g9Ab3g8sIZ').click #navigate to each account
       browser.i(class: 'ico-nav-bar-filter_16px').click
       browser.a(class: 'panel--bordered__item').click
       browser.ul(class: 'radio-group').li(index: 6).click
       browser.button(class: 'button--primary').click
       browser.a(class: '_2wUV-453gB').wait_until(&:present?)
-
+      #get html through Nokogiri object
       account_object = Nokogiri::HTML.parse(browser.html)
-
       #get data for account class object
       name = account_object.at_css("h2.yBcmat9coi").text
       currency = "USD"
       balance = account_object.at_css("div._2tzPNu1unf").text
       nature = "credit_card"
       transactions = Array[]
-
       #parse transactions related data and iterate over it
-      tstate = true
-      if tstate == true
-      transactions_list = account_object.css('.grouped-list--indent')
-      #puts ("\n\n\n")
-      end
-
-      if transactions_list != nil
-        transactions_list.css('.grouped-list__group').each do |li|
-         #get data time for transactions made in one day
+        account_object.css('.grouped-list--indent').css('.grouped-list__group').each do |li|
          date = li.css('.grouped-list__group__heading').text
          #iterate over each day of transactions
-          transactions_list_box = li.css('.grouped-list__group__items')
-          transactions_list_box.css('._2EcJACN7jc').each do |li|
+         li.css('.grouped-list__group__items').css('._2EcJACN7jc').each do |li|
             #get related data for transactions class object
-            description = li.css('.overflow-ellipsis').text
+            description = li.css('.sub-title').text
             amountDebit = li.css('.lQBoxl_Y_x').text
             amountCredit = li.css('._32o6RiLlUL').text
             amountCredit == "" ? amount = "-"+amountDebit[20..-1] : amount = "+"+amountCredit[20..-1]
@@ -80,8 +68,6 @@ class GetAccountsData
             transactions.push(Transactions.new(date,description,amount,currency,name).to_hash)
           end
         end
-      else tstate = false
-      end
       puts ("\n" )
       #define and create account object, include transactions array
       account = Accounts.new(name,currency,balance,nature,transactions).to_hash
